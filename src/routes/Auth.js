645f375c609ef38b7f6839d0115eda901c39d106
kang <rkdguswns0803@gmail.com>
2023-06-28 03:03:13 +0900

    Login form test

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeInput = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitForm}>
+        <input
+          name="email"
+          type="text"
+          placeholder="E-mail"
+          required
+          value={email}
+          onChange={onChangeInput}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          value={password}
+          onChange={onChangeInput}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
