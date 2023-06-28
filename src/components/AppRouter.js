fe0979fd854287bc660ae107ca1fbb7c9ba9fe00
kang <rkdguswns0803@gmail.com>
2023-06-29 03:37:23 +0900

    add Logout, navigation

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
+import Profile from "../routes/Profile";
+import Navigation from "./navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/profile" replace to="/" element={<Profile />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </>
         ) : (
           <Route exact path="/" element={<Auth />} />
